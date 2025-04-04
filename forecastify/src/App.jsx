087eb29198695015ea12f2cb2fb6a81b4d4030bf
soura1334 <sourajeet1334@gmail.com>
2025-04-05 03:23:28 +0530

    Implemented basic Search feature
    Shows locations based on query

--- forecastify/src/App.jsx
+++ forecastify/src/App.jsx
@@ -1,14 +1,46 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 
 export default function App() {
+  const [locations, setLocations] = useState([]);
+  const [query, setQuery] = useState("");
+
+  function handleQuery(val) {
+    setQuery(val);
+  }
+
+  useEffect(
+    function () {
+      async function getLocation() {
+        try {
+          const res = fetch(
+            `https://geocoding-api.open-meteo.com/v1/search?name=${query}&count=5&language=en&format=json`
+          );
+
+          const resp = await res;
+          if (!resp.ok) throw new Error("Unable to fetch");
+          const data = await resp.json();
+
+          setLocations(data.results);
+        } catch (err) {
+          console.error(err.message);
+        }
+      }
+
+      if (!query.length) {
+        setLocations([]);
+        return;
+      }
+
+      getLocation();
+    },
+    [query]
+  );
+
   return (
     <div className="box-border font-mono-Monaco ">
-      <Navbar />
+      <Navbar locations={locations} query={query} onQuery={handleQuery} />
     </div>
   );
 }
-
-
-
-
--- forecastify/src/components/Navbar.jsx
+++ forecastify/src/components/Navbar.jsx
@@ -1,8 +1,8 @@
-export default function Navbar() {
+export default function Navbar({ locations, query, onQuery }) {
   return (
     <header className="bg-[#BBAB8C] grid grid-cols-5 gap-2 p-3">
       <Logo />
-      <Search />
+      <Search locations={locations} query={query} onQuery={onQuery} />
     </header>
   );
 }
@@ -23,17 +23,44 @@ function Logo() {
   );
 }
 
-function Search() {
+function Search({ locations, query, onQuery }) {
   return (
-    <div className="flex justify-center items-center col-span-3 ">
-      <div className="flex border rounded-md items-center p-1">
-        <img src="/src/assets/mag.svg" alt="search icon" className="h-8 p-1" />
-        <input
-          type="search"
-          placeholder="Search a location"
-          className="h-10  text-left p-2 w-100 focus:outline-none"
-        />
-      </div>
+    <div className="flex justify-center items-center col-span-3 relative">
+      <>
+        <div className="flex border rounded-md items-center p-1">
+          <img
+            src="/src/assets/mag.svg"
+            alt="search icon"
+            className="h-8 p-1"
+          />
+          <input
+            type="text"
+            placeholder="Search a location"
+            className="h-10 text-left p-2 w-100 focus:outline-none"
+            value={query}
+            onChange={(e) => onQuery(e.target.value)}
+          />
+        </div>
+        {(locations?.length || 0) > 0 && <SearchRes locations={locations} />}
+      </>
+    </div>
+  );
+}
+
+function SearchRes({ locations }) {
+  return (
+    <div className="border border-gray-400 bg-white absolute z-10 w-110 top-18">
+      {locations.map((loc) => (
+        <SearchData loc={loc} />
+      ))}
+    </div>
+  );
+}
+
+function SearchData({ loc }) {
+  return (
+    <div className="border p-2 hover:bg-gray-200 border-gray-100">
+      {`${loc.name}${loc.name != loc.admin1 ? `, ${loc.admin1}` : ""}, ${loc.country}`}
     </div>
   );
 }
